6fba48c829a663a9ce900fcd4240267637a0e974
jueun036 <jueun036@gmail.com>
2026-08-14 02:02:42 +0900

    feat: JWT 토큰 재발급 및 로그아웃 구현

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import SignupView, LoginView, MeView, LogoutView
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("sessions/", LoginView.as_view(), name="login"),
     path("sessions/current/", LogoutView.as_view(), name="logout"),
+    path("sessions/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", MeView.as_view(), name="me"),
 ]
